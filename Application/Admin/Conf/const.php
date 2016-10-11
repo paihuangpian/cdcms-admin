5ada0677cda84d8d3611af878f3fddf33e0bcab4
MuToRen <1191249254@qq.com>
2016-10-11 13:48:47 +0800

    'BUG修复'

--- Application/Admin/Conf/const.php
+++ Application/Admin/Conf/const.php
@@ -1,7 +1,7 @@
 <?php 
 
 return [
-	'HOST_URL'	=>	[ 'localhost', 'www.cms.com', 'www.cdcms.com', '192.168.1.20', 'www.think.com' ],	
+	'HOST_URL'	=>	[ 'localhost', 'www.cms.com', 'www.cdcms.com', '192.168.1.20', 'www.think.com', 'admin.ydwzjs.cn' ],	
 ];
 
 ?>
